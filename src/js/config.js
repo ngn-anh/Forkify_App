f1486ab34edcc9ff9c08e7b21ef6b38c7c8d1291
Ngân <thungan16092003@gmail.com>
2023-09-25 16:35:24 +0700

    init file

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd28d9f58-4692-421b-a58b-b7a068a38bbf';
+export const MODAL_CLOSE_SEC = 2.5;
